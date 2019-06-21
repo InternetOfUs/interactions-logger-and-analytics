d57163b8f3ae73ced8932f6c6570c8bda0e0954f
Massimiliano Luca <massimiliano.luca.ml@gmail.com>
2019-06-21 16:23:08 +0200

    Version 0.0.16. Library update, data model update and utils update

--- src/memex_logging/models/message.py
+++ src/memex_logging/models/message.py
@@ -1,15 +1,17 @@
 from __future__ import absolute_import, annotations
 
+from typing import Optional, List
+
 
 class Intent:
     def __init__(self, name: str, confidence: float) -> None:
-        self._name = name
-        self._confidence = confidence
+        self.name = name
+        self.confidence = confidence
 
     def to_repr(self) -> dict:
         return {
-            'name': self._name,
-            'confidence': self._confidence
+            'name': self.name,
+            'confidence': self.confidence
         }
 
     @staticmethod
@@ -63,20 +65,20 @@ class AttachmentRequest:
 
     @staticmethod
     def from_rep(data: dict)-> AttachmentRequest:
-        return AttachmentRequest(data['uri'],data['alternativeText'])
+        return AttachmentRequest(data['uri'], data['alternativeText'])
 
 
 class Entity:
     def __init__(self, type: str, value: str, confidence: float):
-        self._type = type
-        self._value = value
-        self._confidence = confidence
+        self.type = type
+        self.value = value
+        self.confidence = confidence
 
     def to_repr(self) -> dict:
         return {
-            'type': self._type,
-            'value': self._value,
-            'confidence': self._confidence
+            'type': self.type,
+            'value': self.value,
+            'confidence': self.confidence
         }
 
     @staticmethod
@@ -86,29 +88,31 @@ class Entity:
 
 class RequestMessage:
 
-    def __init__(self, message_id: str, channel: str, user_id: str, conversation_id:str, timestamp:str, content, domain:str,  intent: Intent, entities: list, project: str,
-                 language: str, metadata:dict) -> None:
-        self._message_id = message_id
-        self._channel = channel
-        self._user_id = user_id
-        self._conversation_id = conversation_id
-        self._timestamp = timestamp
-        self._content = content
-        self._domain = domain
-        self._intent = intent
-        self._entities = entities
-        self._project = project
-        self._language = language
-        self._metadata = metadata
+    def __init__(self, message_id: str, channel: str, user_id: str, conversation_id: str, timestamp: str, content, domain: str,  intent: Intent, entities: list, project: str,
+                 language: str, metadata: dict) -> None:
+        self.message_id = message_id
+        self.channel = channel
+        self.user_id = user_id
+        self.conversation_id = conversation_id
+        self.timestamp = timestamp
+        self.content = content
+        self.domain = domain
+        self.intent = intent
+        self.entities = entities
+        self.project = project
+        self.language = language
+        self.metadata = metadata
 
         if not isinstance(intent, Intent):
             raise ValueError('intent type should be Intent')
 
-        if not isinstance(metadata, dict):
-            raise ValueError('metadata type should be dictionary')
+        if metadata is not None:
+            if not isinstance(metadata, dict):
+                raise ValueError('metadata type should be dictionary')
 
-        if not (isinstance(content, TextualRequest) or isinstance(content, ActionRequest) or isinstance(content, AttachmentRequest)):
-            raise ValueError("content should be TextRequest, ActionRequest or AttachmentRequest")
+        if content is not None:
+            if not (isinstance(content, TextualRequest) or isinstance(content, ActionRequest) or isinstance(content, AttachmentRequest)):
+                raise ValueError("content should be TextRequest, ActionRequest or AttachmentRequest")
 
         for entity in entities:
             print(type(entity))
@@ -117,21 +121,32 @@ class RequestMessage:
 
     def to_repr(self) -> dict:
         entities = []
-        for entity in self._entities:
-            entities.append(entity.to_repr())
+        for entity in self.entities:
+            entities.append(entity.to_repr)
+
+        local_content = None
+
+        if self.content is not None:
+            local_content = self.content.to_repr()
+
+        local_intent = None
+
+        if self.intent is not None:
+            local_intent = self.intent.to_repr()
+
         return {
-            'messageId': self._message_id,
-            'channel': self._channel,
-            'userId': self._user_id,
-            'conversationId': self._conversation_id,
-            'timestamp': self._timestamp,
-            'content': self._content.to_repr(),
-            'domain': self._domain,
-            'intent': self._intent.to_repr(),
+            'messageId': self.message_id,
+            'channel': self.channel,
+            'userId': self.user_id,
+            'conversationId': self.conversation_id,
+            'timestamp': self.timestamp,
+            'content': local_content,
+            'domain': self.domain,
+            'intent': local_intent,
             'entities': entities,
-            'project': self._project,
-            'language': self._language,
-            'metadata': self._metadata,
+            'project': self.project,
+            'language': self.language,
+            'metadata': self.metadata,
             'type': 'REQUEST'
         }
 
@@ -141,28 +156,30 @@ class RequestMessage:
             intent = Intent.from_rep(data['intent'])
         else:
             intent_value = {
-                'name': 'nan',
-                'confidence': 0
+                'name': None,
+                'confidence': 0.0
             }
             intent = Intent.from_rep(intent_value)
 
         if 'domain' in data:
             domain = data['domain']
         else:
-            domain = "undefined"
+            domain = None
 
         if 'conversationId' in data:
             conversation_id = data['conversationId']
         else:
-            conversation_id = "undefined"
+            conversation_id = None
 
-        if data['content']['type'] == "text":
-            content = TextualRequest.from_rep(data['content'])
-        elif data['content']['type'] == "action":
-            content = ActionRequest.from_rep(data['content'])
-        elif data['content']['type'] == "attachment":
-            content = AttachmentRequest.from_rep(data['content'])
+        content = None
 
+        if "content" in data:
+            if data['content']['type'] == "text":
+                content = TextualRequest.from_rep(data['content'])
+            elif data['content']['type'] == "action":
+                content = ActionRequest.from_rep(data['content'])
+            elif data['content']['type'] == "attachment":
+                content = AttachmentRequest.from_rep(data['content'])
 
         entities = []
         if "entities" in data:
@@ -170,26 +187,34 @@ class RequestMessage:
                 e = Entity.from_rep(entity)
                 entities.append(e)
 
-        return RequestMessage(data['messageId'], data['channel'], data['userId'],conversation_id,data['timestamp'],content,domain, intent, entities,
-                              data['project'], data['language'], data['metadata'])
+        metadata = None
+        if "metadata" in data:
+            metadata = data['metadata']
+
+        language = None
+        if "language" in data:
+            language = data['language']
+
+        return RequestMessage(data['messageId'], data['channel'], data['userId'], conversation_id, data['timestamp'], content, domain, intent, entities,
+                              data['project'], language, metadata)
 
 
 class ResponseMessage:
-    def __init__(self, message_id: str, conversation_id: str, channel: str, user_id:str, response_to:str, timestamp:str, content:list, metadata:dict, project:str):
+    def __init__(self, message_id: str, conversation_id: str, channel: str, user_id: str, response_to: str, timestamp: str, content: list, metadata: dict, project: str):
 
-        self._message_id = message_id
-        self._conversation_id = conversation_id
-        self._channel = channel
-        self._user_id = user_id
-        self._response_to = response_to
-        self._timestamp = timestamp
-        self._content = content
-        self._metadata = metadata
-        self._project = project
-        self._type = "RESPONSE"
+        self.message_id = message_id
+        self.conversation_id = conversation_id
+        self.channel = channel
+        self.user_id = user_id
+        self.response_to = response_to
+        self.timestamp = timestamp
+        self.content = content
+        self.metadata = metadata
+        self.project = project
+        self.type = "RESPONSE"
 
         for cont in content:
-            if not (isinstance(cont, QuickReplyResponse) or isinstance(cont,CarouselResponse) or isinstance(cont, AttachmentResponse) or isinstance(cont,TextResponse)):
+            if not (isinstance(cont, QuickReplyResponse) or isinstance(cont, CarouselResponse) or isinstance(cont, AttachmentResponse) or isinstance(cont, TextResponse)):
                 raise ValueError("response should contains only elements from QuickReplyResponse, CarouselResponse, AttachmentResponse, TextResponse")
 
         if not isinstance(metadata, dict):
@@ -197,25 +222,31 @@ class ResponseMessage:
 
     def to_repr(self) -> dict:
         content_list = []
-        for c in self._content:
+        for c in self.content:
             content_list.append(c.to_repr())
 
         return {
-            'messageId': self._message_id,
-            'conversationId': self._conversation_id,
-            'channel': self._channel,
-            'userId': self._user_id,
-            'responseTo': self._response_to,
-            'timestamp': self._timestamp,
+            'messageId': self.message_id,
+            'conversationId': self.conversation_id,
+            'channel': self.channel,
+            'userId': self.user_id,
+            'responseTo': self.response_to,
+            'timestamp': self.timestamp,
             'content': content_list,
-            'metadata': self._metadata,
-            'project': self._project,
-            'type': self._type
+            'metadata': self.metadata,
+            'project': self.project,
+            'type': self.type
         }
 
     @staticmethod
     def from_rep(data: dict):
+
+        metadata = None
+        if "metadata" in data:
+            metadata = data["metadata"]
+
         content = []
+
         for item in data['content']:
             if item['type'] == 'text':
                 element = TextResponse.from_rep(item)
@@ -230,24 +261,24 @@ class ResponseMessage:
                 element = CarouselResponse.from_rep(item)
                 content.append(element)
 
-        return ResponseMessage(data['messageId'], data['conversationId'], data['channel'], data['userId'], data['responseTo'], data['timestamp'], content, data['metadata'], data['project'])
+        return ResponseMessage(data['messageId'], data['conversationId'], data['channel'], data['userId'], data['responseTo'], data['timestamp'], content, metadata, data['project'])
 
 
 class NotificationMessage:
-    def __init__(self, message_id: str, conversation_id: str, channel: str, user_id:str, timestamp:str, content:list, metadata:dict, project:str):
+    def __init__(self, message_id: str, conversation_id: str, channel: str, user_id: str, timestamp: str, content: list, metadata: dict, project: str):
 
-        self._message_id = message_id
-        self._conversation_id = conversation_id
-        self._channel = channel
-        self._user_id = user_id
-        self._timestamp = timestamp
-        self._content = content
-        self._metadata = metadata
-        self._project = project
-        self._type = type
+        self.message_id = message_id
+        self.conversation_id = conversation_id
+        self.channel = channel
+        self.user_id = user_id
+        self.timestamp = timestamp
+        self.content = content
+        self.metadata = metadata
+        self.project = project
+        self.type = type
 
         for cont in content:
-            if not (isinstance(cont, QuickReplyResponse) or isinstance(cont,CarouselResponse) or isinstance(cont, AttachmentResponse) or isinstance(cont,TextResponse)):
+            if not (isinstance(cont, QuickReplyResponse) or isinstance(cont, CarouselResponse) or isinstance(cont, AttachmentResponse) or isinstance(cont, TextResponse)):
                 raise ValueError("response should contains only elements from QuickReplyResponse, CarouselResponse, AttachmentResponse, TextResponse")
 
         if not isinstance(metadata, dict):
@@ -255,23 +286,28 @@ class NotificationMessage:
 
     def to_repr(self) -> dict:
         content_list = []
-        for c in self._content:
-            content_list.append(c.to_repr())
+        for c in self.content:
+            content_list.append(c.to_repr)
 
         return {
-            'messageId': self._message_id,
-            'conversationId': self._conversation_id,
-            'channel': self._channel,
-            'userId': self._user_id,
-            'timestamp': self._timestamp,
+            'messageId': self.message_id,
+            'conversationId': self.conversation_id,
+            'channel': self.channel,
+            'userId': self.user_id,
+            'timestamp': self.timestamp,
             'content': content_list,
-            'metadata': self._metadata,
-            'project': self._project,
-            'type': self._type
+            'metadata': self.metadata,
+            'project': self.project,
+            'type': self.type
         }
 
     @staticmethod
     def from_rep(data: dict):
+
+        metadata = None
+        if "metadata" in data:
+            metadata = data["metadata"]
+
         content = []
         for item in data['content']:
             if item['type'] == 'text':
@@ -287,15 +323,59 @@ class NotificationMessage:
                 element = CarouselResponse.from_rep(item)
                 content.append(element)
 
-        return NotificationMessage(data['messageId'], data['conversationId'], data['channel'], data['userId'], data['timestamp'], content, data['metadata'], data['project'])
+        return NotificationMessage(data['messageId'], data['conversationId'], data['channel'], data['userId'], data['timestamp'], content, metadata, data['project'])
+
+
+class QuickReplyResponse:
+    def __init__(self, button_text: str, button_id: str, media_type: str, media_uri: str):
+        self.type = "action"
+        self.button_text = button_text
+        self.button_id = button_id
+        self.media_type = media_type
+        self.media_uri = media_uri
+
+    def to_repr(self) -> dict:
+        return {
+            'type': 'action',
+            'buttonText': self.button_text,
+            'buttonId': self.button_id,
+            'mediaType': self.media_type,
+            'mediaURI': self.media_uri
+        }
+
+    @staticmethod
+    def from_rep(data: dict):
+
+        button_text = None
+
+        if "buttonText" in data:
+            button_text = data['buttonText']
+
+        button_id = None
+
+        if "buttonId" in data:
+            button_id = data['buttonId']
+
+        media_type = None
+
+        if "mediaType" in data:
+            media_type = data['mediaType']
+
+        media_uri = None
+
+        if "mediaURI" in data:
+            media_uri = data['mediaURI']
+
+        return QuickReplyResponse(button_text, button_id, media_type, media_uri)
+
 
 class AttachmentResponse:
-    def __init__(self, uri: str, alternative_text: str, buttons=[]):
+    def __init__(self, uri: str, alternative_text="", buttons=Optional[List[QuickReplyResponse]]):
         """
         Create an AttachmentResponse Object. An attachment response is a response containing only a media
         :param uri: uri of the media as a string
         :param alternative_text: the alternative text of the media. It is a string and it is displayed whenever the media cannot be loaded correctly. Usually, it is a description of the media
-        :param buttons: a list of QuickReply responses (list of buttons to let the user perform quick actions)
+        :param buttons: a list of QuickReply responses (list of buttons to let the user perform quick actions). This field is optional
         """
 
         self.uri = uri
@@ -327,16 +407,16 @@ class AttachmentResponse:
         for action in data['action']:
             a = QuickReplyResponse.from_rep(action)
             actions.append(a)
-        return AttachmentResponse(data['uri'],data['alternativeText'],actions)
+        return AttachmentResponse(data['uri'], data['alternativeText'], actions)
 
 
 class CarouselResponse:
-    def __init__(self, type, title, description, uri, actions:list):
-        self._type = type,
-        self._title = title,
-        self._description = description,
-        self._uri = uri,
-        self._actions = actions
+    def __init__(self, type, title, description, uri, actions=Optional[List[QuickReplyResponse]]):
+        self.type = type,
+        self.title = title,
+        self.description = description,
+        self.uri = uri,
+        self.actions = actions
 
         for action in actions:
             if not isinstance(action, QuickReplyResponse):
@@ -344,13 +424,13 @@ class CarouselResponse:
 
     def to_repr(self) -> dict:
         actions = []
-        for action in self._actions:
+        for action in self.actions:
             actions.append(QuickReplyResponse.to_repr(action))
         return{
-            'type': self._type,
-            'title': self._title,
-            'description': self._description,
-            'uri': self._uri,
+            'type': self.type,
+            'title': self.title,
+            'description': self.description,
+            'uri': self.uri,
             'actions': actions
         }
 
@@ -366,39 +446,15 @@ class CarouselResponse:
 
 class TextResponse:
     def __init__(self, type, value) -> None:
-        self._type = type
-        self._value = value
+        self.type = type
+        self.value = value
 
     def to_repr(self) -> dict:
         return {
-            'type': self._type,
-            'value': self._value
+            'type': self.type,
+            'value': self.value
         }
 
     @staticmethod
     def from_rep(data: dict):
         return TextResponse(data['type'], data['value'])
-
-
-class QuickReplyResponse:
-    def __init__(self, type:str, button_text: str, button_id: str, media_type: str, media_uri: str):
-        self._type = type
-        self._button_text = button_text
-        self._button_id = button_id
-        self._media_type = media_type
-        self._media_uri = media_uri
-
-    def to_repr(self) -> dict:
-        return {
-            'type': self._type,
-            'buttonText': self._button_text,
-            'buttonId': self._button_id,
-            'mediaType': self._media_type,
-            'mediaURI': self._media_uri
-        }
-
-    @staticmethod
-    def from_rep(data: dict):
-        return QuickReplyResponse(data['type'], data['buttonText'], data['buttonId'], data['mediaType'], data['mediaURI'])
-
-
--- src/memex_logging/ws_memex/messages_api/main.py
+++ src/memex_logging/ws_memex/messages_api/main.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 
-from flask import request
+import json
+
+from flask import request, Response
 from flask_restful import Resource, abort
 
 from datetime import datetime
@@ -26,10 +28,10 @@ class ManipulateMessage(Resource):
     This class can be used to log a single message. The only message allowed is post
     """
 
-    def __init__(self, es: Elasticsearch):
+    def __init__(self, es: Elasticsearch) -> None:
         self._es = es
 
-    def delete(self, project, messageId) -> tuple:
+    def delete(self, project: str, messageId: str) -> Response:
         """
         Method to delete a message of a specific project by id
         :param project: the name of the project
@@ -47,13 +49,16 @@ class ManipulateMessage(Resource):
         self._es.delete_by_query(index=index, body=query)
 
         json_response = {
-            "trace_id": messageId,
+            "messageId": messageId,
             "action": "deleted",
             "code": 200
         }
-        return json_response, 200
+        resp = Response(json.dumps(json_response), mimetype='application/json')
+        resp.status_code = 200
 
-    def get(self, project, messageId)-> tuple:
+        return resp
+
+    def get(self, project, messageId)-> Response:
         """
         Method to obtain a message of a specific project by id
         :param project: the name of the project
@@ -72,7 +77,10 @@ class ManipulateMessage(Resource):
         if response['hits']['total'] == 0:
             abort(404, message="resource not found")
         else:
-            return response['hits']['hits'][0]['_source'], 200
+            resp = Response(json.dumps(response['hits']['hits'][0]['_source']), mimetype='application/json')
+            resp.status_code = 200
+
+            return resp
 
 
 class LogMessages(Resource):
@@ -83,39 +91,47 @@ class LogMessages(Resource):
     def __init__(self, es: Elasticsearch):
         self._es = es
 
-    def post(self) -> tuple:
+    def post(self) -> Response:
         """
         Add a batch of messages to the database. Pass a JSON array in the body of the request
         :return: the HTTP response
         """
         messages_received = request.json
-        print(len(messages_received))
+        message_ids = []
+
         for message in messages_received:
             type = str(message['type']).lower()
             utils = Utils()
 
             if type == 'request':
                 request_message = RequestMessage.from_rep(message)
+                message_ids.append(request_message.message_id)
                 project_name = utils.extract_project_name(message)
-                index_name = project_name + "-message-" + datetime.today().strftime('%Y-%m-%d')
+                date = datetime.today().strftime('%Y-%m-%d')
+                index_name = project_name + "-message-" + date
                 self._es.index(index=index_name, doc_type='_doc', body=request_message.to_repr())
 
             elif type == "response":
                 response_message = ResponseMessage.from_rep(message)
+                message_ids.append(response_message.message_id)
                 project_name = utils.extract_project_name(message)
                 index_name = project_name + "-message-" + datetime.today().strftime('%Y-%m-%d')
                 self._es.index(index=index_name, doc_type='_doc', body=response_message.to_repr())
 
             elif type == "notification":
                 notification_message = NotificationMessage.from_rep(message)
+                message_ids.append(notification_message.message_id)
                 project_name = utils.extract_project_name(message)
                 index_name = project_name + "-message-" + datetime.today().strftime('%Y-%m-%d')
                 self._es.index(index=index_name, doc_type='_doc', body=notification_message.to_repr())
 
         json_response = {
-            "text": "messages added",
+            "messageId": message_ids,
             "status": "ok",
             "code": 200
         }
 
-        return json_response, 200
+        resp = Response(json.dumps(json_response), mimetype='application/json')
+        resp.status_code = 200
+
+        return resp
