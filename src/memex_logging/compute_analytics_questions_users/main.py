a3af4a49cee0dbcb36adb7ad7c31bb5f2e76a9e7
Samuel Bortolin <samuel.bortolin@u-hopper.com>
2021-03-18 17:00:10 +0100

    Refactored the script for the analytics and fixed logging utils adding also the get messages method. Included ilog usage information in the user file and removed the wenet id

--- src/memex_logging/compute_analytics_questions_users/main.py
+++ src/memex_logging/compute_analytics_questions_users/main.py
@@ -61,6 +61,7 @@ if __name__ == '__main__':
     arg_parser.add_argument("-i", "--instance", type=str, default=os.getenv("INSTANCE", "https://wenet.u-hopper.com/dev"), help="The target WeNet instance")
     arg_parser.add_argument("-a", "--apikey", type=str, default=os.getenv("APIKEY"), help="The apikey for accessing the services")
     arg_parser.add_argument("-ai", "--appid", type=str, default=os.getenv("APP_ID"), help="The id of the application in which compute the analytics")
+    arg_parser.add_argument("-il", "--ilog", type=str, default=os.getenv("ILOG"), help="The id of the ilog application to check if the user has enabled it or not")
     arg_parser.add_argument("-p", "--project", type=str, default=os.getenv("PROJECT"), help="The project for which to compute the analytics")
     arg_parser.add_argument("-r", "--range", type=str, default=os.getenv("TIME_RANGE", "30D"), help="The temporal range in which compute the analytics")
     arg_parser.add_argument("-s", "--start", type=str, default=os.getenv("START_TIME"), help="The start time from which compute the analytics")
@@ -90,36 +91,37 @@ if __name__ == '__main__':
     created_to = datetime.fromisoformat(created_to)
 
     name, extension = os.path.splitext(args.afile)
+    analytics_file = open(args.afile, "w")
     if extension == ".csv":
-        analytics_file = csv.writer(open(args.afile, "w"))
+        analytics_file_writer = csv.writer(analytics_file)
     elif extension == ".tsv":
-        analytics_file = csv.writer(open(args.afile, "w"), delimiter="\t")
+        analytics_file_writer = csv.writer(analytics_file, delimiter="\t")
     else:
         logger.warning(f"For the analytics, you should pass the path of one of the following type of file [.csv, .tsv], instead you pass [{extension}]")
         raise ValueError(f"For the analytics, you should pass the path of one of the following type of file [.csv, .tsv], instead you pass [{extension}]")
 
-    analytics_file.writerow(["app id", args.appid])
-    analytics_file.writerow(["project", args.project])
-    analytics_file.writerow(["from", created_from])
-    analytics_file.writerow(["to", created_to])
-    analytics_file.writerow([])
-    analytics_file.writerow(["metric", "count", "description"])
+    analytics_file_writer.writerow(["app id", args.appid])
+    analytics_file_writer.writerow(["project", args.project])
+    analytics_file_writer.writerow(["from", created_from])
+    analytics_file_writer.writerow(["to", created_to])
+    analytics_file_writer.writerow([])
+    analytics_file_writer.writerow(["metric", "count", "description"])
 
     total_users = UserMetric("u:total")
     total_users_result = operations.get_analytic(time_range, total_users)
-    analytics_file.writerow(["total users", total_users_result["count"], "The total number of users of the application"])
+    analytics_file_writer.writerow(["total users", total_users_result["count"], "The total number of users of the application"])
 
     active_users = UserMetric("u:active")
     active_users_result = operations.get_analytic(time_range, active_users)
-    analytics_file.writerow(["active users", active_users_result["count"], "The number of users who used the application"])
+    analytics_file_writer.writerow(["active users", active_users_result["count"], "The number of users who used the application"])
 
     engaged_users = UserMetric("u:engaged")
     engaged_users_result = operations.get_analytic(time_range, engaged_users)
-    analytics_file.writerow(["engaged users", engaged_users_result["count"], "The number of users who received a notification from the platform (incentive, prompt, badge, ..)"])
+    analytics_file_writer.writerow(["engaged users", engaged_users_result["count"], "The number of users who received a notification from the platform (incentive, prompt, badge, ..)"])
 
     new_users = UserMetric("u:new")
     new_users_result = operations.get_analytic(time_range, new_users)
-    analytics_file.writerow(["new users", new_users_result["count"], "The number of new users who activated the application during the period of this analysis"])
+    analytics_file_writer.writerow(["new users", new_users_result["count"], "The number of new users who activated the application during the period of this analysis"])
 
     segmentation_messages = MessageMetric("m:segmentation")
     segmentation_messages_result = operations.get_analytic(time_range, segmentation_messages)
@@ -127,12 +129,12 @@ if __name__ == '__main__':
     total_messages = 0
     for key in segmentation_messages_result["counts"]:
         total_messages += segmentation_messages_result["counts"][key]
-    analytics_file.writerow(["total messages", total_messages, "The total number of messages exchanged"])
+    analytics_file_writer.writerow(["total messages", total_messages, "The total number of messages exchanged"])
 
     request_messages = 0
     if TYPE_REQUEST_MESSAGE in segmentation_messages_result["counts"]:
         request_messages = segmentation_messages_result["counts"][TYPE_REQUEST_MESSAGE]
-    analytics_file.writerow(["messages from users", request_messages, "The number of messages sent by users (textual messages, clicked buttons and commands)"])
+    analytics_file_writer.writerow(["messages from users", request_messages, "The number of messages sent by users (textual messages, clicked buttons and commands)"])
 
     segmentation_requests = MessageMetric("r:segmentation")
     segmentation_requests_result = operations.get_analytic(time_range, segmentation_requests)
@@ -140,25 +142,25 @@ if __name__ == '__main__':
     text_requests = 0
     if TYPE_TEXT_CONTENT_REQUEST in segmentation_requests_result["counts"]:
         text_requests = segmentation_requests_result["counts"][TYPE_TEXT_CONTENT_REQUEST]
-    analytics_file.writerow(["user textual messages", text_requests, "The number of textual messages sent by the users"])
+    analytics_file_writer.writerow(["user textual messages", text_requests, "The number of textual messages sent by the users"])
 
     action_requests = 0
     if TYPE_ACTION_CONTENT_REQUEST in segmentation_requests_result["counts"]:
         action_requests = segmentation_requests_result["counts"][TYPE_ACTION_CONTENT_REQUEST]
-    analytics_file.writerow(["user action messages", action_requests, "The number of action messages (buttons and commands) sent by the users"])
+    analytics_file_writer.writerow(["user action messages", action_requests, "The number of action messages (buttons and commands) sent by the users"])
 
     response_messages = 0
     if TYPE_RESPONSE_MESSAGE in segmentation_messages_result["counts"]:
         response_messages = segmentation_messages_result["counts"][TYPE_RESPONSE_MESSAGE]
-    analytics_file.writerow(["messages from bot", response_messages, "The number of messages sent by the application"])
+    analytics_file_writer.writerow(["messages from bot", response_messages, "The number of messages sent by the application"])
 
     notification_messages = 0
     if TYPE_NOTIFICATION_MESSAGE in segmentation_messages_result["counts"]:
         notification_messages = segmentation_messages_result["counts"][TYPE_NOTIFICATION_MESSAGE]
-    analytics_file.writerow(["messages from wenet", notification_messages, "The number of messages sent by the WeNet platform"])
+    analytics_file_writer.writerow(["messages from wenet", notification_messages, "The number of messages sent by the WeNet platform"])
 
     tasks = task_manager_connector.get_tasks(args.appid, created_from, created_to)
-    analytics_file.writerow(["questions ", len(tasks), "The number of questions asked by the users"])
+    analytics_file_writer.writerow(["questions", len(tasks), "The number of questions asked by the users"])
 
     transactions = task_manager_connector.get_transactions(args.appid, created_from, created_to)
     # file.writerow(["actions on questions and answers", len(transactions), "The total number of actions performed on questions and answers by the users"])
@@ -169,39 +171,43 @@ if __name__ == '__main__':
     # file.writerow(["created questions", created_task_transactions, "The number of questions created"])  # it should be equal to `questions`, it is a duplicate
 
     report_question_transactions = transaction_labels.count(LABEL_REPORT_QUESTION_TRANSACTION)
-    analytics_file.writerow(["question reports", report_question_transactions, "The number of reports created for questions (e.g. usually for malicious contents)"])
+    analytics_file_writer.writerow(["question reports", report_question_transactions, "The number of reports created for questions (e.g. usually for malicious contents)"])
 
     not_answer_transactions = transaction_labels.count(LABEL_NOT_ANSWER_TRANSACTION)
-    analytics_file.writerow(["can't help", not_answer_transactions, "The number of times a user cannot answer a question (by clicking on the button “Can't help”)"])
+    analytics_file_writer.writerow(["can't help", not_answer_transactions, "The number of times a user cannot answer a question (by clicking on the button “Can't help”)"])
 
     answer_transactions = transaction_labels.count(LABEL_ANSWER_TRANSACTION)
-    analytics_file.writerow(["answer", answer_transactions, "The number of answers provided by the users"])
+    analytics_file_writer.writerow(["answers", answer_transactions, "The number of answers provided by the users"])
 
     report_answer_transactions = transaction_labels.count(LABEL_REPORT_ANSWER_TRANSACTION)
-    analytics_file.writerow(["answer reports", report_answer_transactions, "The number of reports created for answers (e.g. usually for malicious contents)"])
+    analytics_file_writer.writerow(["answer reports", report_answer_transactions, "The number of reports created for answers (e.g. usually for malicious contents)"])
 
     best_answer_transactions = transaction_labels.count(LABEL_BEST_ANSWER_TRANSACTION)
-    analytics_file.writerow(["approved answers", best_answer_transactions, "The number answers accepted as the correct/useful ones"])
+    analytics_file_writer.writerow(["approved answers", best_answer_transactions, "The number answers accepted as the correct/useful ones"])
 
     more_answer_transactions = transaction_labels.count(LABEL_MORE_ANSWER_TRANSACTION)
-    analytics_file.writerow(["ask more people", more_answer_transactions, "The number of times a user asked WeNet for more answers (by clicking on the button “Ask more people”)"])
+    analytics_file_writer.writerow(["ask more people", more_answer_transactions, "The number of times a user asked WeNet for more answers (by clicking on the button “Ask more people”)"])
+
+    analytics_file.close()
 
     # all questions and their chosen answers
     name, extension = os.path.splitext(args.qfile)
+    questions_file = open(args.qfile, "w")
     if extension == ".csv":
-        questions_file = csv.writer(open(args.qfile, "w"))
+        questions_file_writer = csv.writer(questions_file)
     elif extension == ".tsv":
-        questions_file = csv.writer(open(args.qfile, "w"), delimiter="\t")
+        questions_file_writer = csv.writer(questions_file, delimiter="\t")
     else:
         logger.warning(f"For the questions, you should pass the path of one of the following type of file [.csv, .tsv], instead you pass [{extension}]")
         raise ValueError(f"For the questions, you should pass the path of one of the following type of file [.csv, .tsv], instead you pass [{extension}]")
 
-    questions_file.writerow(["app id", args.appid])
-    questions_file.writerow(["project", args.project])
-    questions_file.writerow(["from", created_from])
-    questions_file.writerow(["to", created_to])
-    questions_file.writerow([])
-    questions_file.writerow(["question", "answer"])
+    questions_file_writer.writerow(["app id", args.appid])
+    questions_file_writer.writerow(["project", args.project])
+    questions_file_writer.writerow(["from", created_from])
+    questions_file_writer.writerow(["to", created_to])
+    questions_file_writer.writerow(["total questions", len(tasks)])
+    questions_file_writer.writerow([])
+    questions_file_writer.writerow(["question", "answer"])
 
     for task in tasks:
         id_answer_map = {}
@@ -211,28 +217,30 @@ if __name__ == '__main__':
                 id_answer_map[transaction.id] = transaction.attributes.get("answer")
             if transaction.label == LABEL_BEST_ANSWER_TRANSACTION:
                 chosen_answer_id = transaction.attributes.get("transactionId")
-        if chosen_answer_id:
-            questions_file.writerow([task.goal.name, id_answer_map[chosen_answer_id]])
-        else:
-            questions_file.writerow([task.goal.name])
+        questions_file_writer.writerow([task.goal.name, id_answer_map[chosen_answer_id] if chosen_answer_id else None])
+
+    questions_file.close()
 
     # pilot users and associated cohorts
     name, extension = os.path.splitext(args.ufile)
+    users_file = open(args.ufile, "w")
     if extension == ".csv":
-        users_file = csv.writer(open(args.ufile, "w"))
+        users_file_writer = csv.writer(users_file)
     elif extension == ".tsv":
-        users_file = csv.writer(open(args.ufile, "w"), delimiter="\t")
+        users_file_writer = csv.writer(users_file, delimiter="\t")
     else:
         logger.warning(f"For the users, you should pass the path of one of the following type of file [.csv, .tsv], instead you pass [{extension}]")
         raise ValueError(f"For the users, you should pass the path of one of the following type of file [.csv, .tsv], instead you pass [{extension}]")
 
-    users_file.writerow(["app id", args.appid])
-    users_file.writerow(["project", args.project])
-    users_file.writerow([])
-    users_file.writerow(["user id", "name", "surname", "email", "gender", "incentive cohort"])
-
+    users_file_writer.writerow(["app id", args.appid])
+    users_file_writer.writerow(["project", args.project])
     user_ids = hub_interface.get_user_ids_for_app(args.appid)
+    users_file_writer.writerow(["total users", len(user_ids)])
+    users_file_writer.writerow([])
+    users_file_writer.writerow(["name", "surname", "email", "gender", "incentive cohort", "ilog"])
+
     cohorts = incentive_server_connector.get_cohorts()
+    ilog_user_ids = hub_interface.get_user_ids_for_app(args.ilog)
     for user_id in user_ids:
         user = profile_manager_connector.get_user_profile(user_id)
         user_cohort = None
@@ -243,7 +251,14 @@ if __name__ == '__main__':
                         user_cohort = "badges"
                     elif cohort.get("cohort") == 1:
                         user_cohort = "incentives and badges"
-        if user_cohort:
-            users_file.writerow([user.profile_id, user.name.first, user.name.last, user.email, user.gender.name, user_cohort])
-        else:
-            users_file.writerow([user.profile_id, user.name.first, user.name.last, user.email, user.gender.name])
+                    break
+
+        has_user_enabled_ilog = "no"
+        for ilog_user_id in ilog_user_ids:
+            if ilog_user_id == user_id:
+                has_user_enabled_ilog = "yes"
+                break
+
+        users_file_writer.writerow([user.name.first, user.name.last, user.email, user.gender.name.lower() if user.gender else None, user_cohort, has_user_enabled_ilog])
+
+    users_file.close()
--- src/memex_logging/memex_logging_lib/logging_utils.py
+++ src/memex_logging/memex_logging_lib/logging_utils.py
@@ -16,6 +16,7 @@ from __future__ import absolute_import, annotations
 
 import json
 import logging
+from datetime import datetime
 from time import sleep
 from typing import Optional, List, Union
 
@@ -549,7 +550,7 @@ class LoggingUtility:
 
     def add_quick_reply_response(self, buttons: list, message_id: str, channel: str, user_id: str,
                                  response_to: str, timestamp: str, conversation_id=None,
-                                 metadata=Optional[dict]) -> tuple:
+                                 metadata=Optional[dict]) -> str:
         """
         This method should be used to store a response message. A response message is a message from the bot to the user that represents an answer to a request message
         :param buttons: the list of the buttons
@@ -860,7 +861,7 @@ class LoggingUtility:
 
     def add_quick_reply_notification(self, buttons: list, message_id: str, channel: str, user_id: str,
                                      timestamp: str, conversation_id=None,
-                                     metadata=Optional[dict]) -> tuple:
+                                     metadata=Optional[dict]) -> str:
         """
         This method should be used to store a notification message.
         :param buttons: the list of the buttons
@@ -1000,7 +1001,7 @@ class LoggingUtility:
         else:
             raise ValueError("The message has not been logged")
 
-    def get_message_from_message_id_and_user_id(self, message_id: str, user_id: str) -> tuple:
+    def get_message_from_message_id_and_user_id(self, message_id: str, user_id: str) -> dict:
         """
         Utils to retrieve a message from the database
         :param message_id: the if of the message to retrieve
@@ -1011,9 +1012,12 @@ class LoggingUtility:
 
         response = requests.get(api_point, headers=self._custom_headers)
 
-        return response.status_code, response.json()
+        if response.status_code == 200:
+            return json.loads(response.content)
+        else:
+            raise ValueError("Cannot retrieve the message")
 
-    def get_message_from_trace_id(self, trace_id: str) -> tuple:
+    def get_message_from_trace_id(self, trace_id: str) -> dict:
         """
         Utils to retrieve a message from the database
         :param trace_id: the traceId of the message to retrieve
@@ -1023,9 +1027,41 @@ class LoggingUtility:
 
         response = requests.get(api_point, headers=self._custom_headers)
 
-        return response.status_code, response.json()
+        if response.status_code == 200:
+            return json.loads(response.content)
+        else:
+            raise ValueError("Cannot retrieve the message")
 
-    def delete_message_from_message_id_and_user_id(self, message_id: str, user_id: str) -> tuple:
+    def get_messages(self, from_time: datetime, to_time: datetime, user_id: str = None, channel: str = None, message_type: str = None, max_size: int = 1000) -> list:
+        """
+        Utils to retrieve a message from the database
+        :param from_time: the time from which retrieve the messages
+        :param to_time:  the time up to which retrieve the messages
+        :param user_id: the id of the user related to the messages to retrieve
+        :param channel: the channel of the messages to retrieve
+        :param message_type: the type of the messages to retrieve
+        :param max_size: the maximum number of messages to retrieve (up to 10000)
+        :return: the status and the content of the response
+        """
+        api_point = self._access_point + "/messages?project=" + self._project + "&fromTime=" + from_time.isoformat() + "&toTime=" + to_time.isoformat() + "&maxSize=" + str(max_size)
+
+        if user_id:
+            api_point = api_point + "&userId=" + user_id
+
+        if channel:
+            api_point = api_point + "&channel=" + channel
+
+        if message_type:
+            api_point = api_point + "&type=" + message_type
+
+        response = requests.get(api_point, headers=self._custom_headers)
+
+        if response.status_code == 200:
+            return json.loads(response.content)
+        else:
+            raise ValueError("Cannot retrieve the messages")
+
+    def delete_message_from_message_id_and_user_id(self, message_id: str, user_id: str) -> dict:
         """
         Utils to delete a message from the database
         :param message_id: the if of the message to delete
@@ -1036,9 +1072,12 @@ class LoggingUtility:
 
         response = requests.delete(api_point, headers=self._custom_headers)
 
-        return response.status_code, response.json()
+        if response.status_code == 200:
+            return json.loads(response.content)
+        else:
+            raise ValueError("Cannot delete the message")
 
-    def delete_message_from_trace_id(self, trace_id: str) -> tuple:
+    def delete_message_from_trace_id(self, trace_id: str) -> dict:
         """
         Utils to delete a message from the database
         :param trace_id: the traceId of the message to delete
@@ -1048,7 +1087,10 @@ class LoggingUtility:
 
         response = requests.delete(api_point, headers=self._custom_headers)
 
-        return response.status_code, response.json()
+        if response.status_code == 200:
+            return json.loads(response.content)
+        else:
+            raise ValueError("Cannot delete the message")
 
     def add_log(self, log_id: str, component: str, severity: str, log_content: str, timestamp: str, authority: str = None,  bot_version: str = None, metadata: dict = None) -> str:
 
@@ -1093,9 +1135,9 @@ class LoggingUtility:
 
         if response.status_code == 200:
             dict_response = json.loads(response.text)
-            return dict_response['logId']
+            return dict_response['logId'][0]
         else:
-            raise ValueError("The message has not been logged")
+            raise ValueError("The log has not been logged")
 
     def get_analytic(self, temporal_range: Union[DefaultTime, CustomTime], metric: Metric, sleep_time: int = 1) -> dict:
 
