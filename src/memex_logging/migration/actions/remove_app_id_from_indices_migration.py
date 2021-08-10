84e84945d8e9db201dcf082472bb58fa28171994
Samuel Bortolin <samuel.bortolin@u-hopper.com>
2021-08-10 14:35:10 +0200

    Used bulk operations

--- src/memex_logging/migration/actions/remove_app_id_from_indices_migration.py
+++ src/memex_logging/migration/actions/remove_app_id_from_indices_migration.py
@@ -17,7 +17,7 @@ from __future__ import absolute_import, annotations
 from datetime import datetime
 
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import scan
+from elasticsearch.helpers import scan, bulk
 
 from memex_logging.migration.migration import MigrationAction
 
@@ -47,10 +47,17 @@ class RemoveAppIdFromIndicesMigration(MigrationAction):
 
         for index in es.indices.get('analytic-*'):
             analytics = scan(es, index=index, query={"query": {"match_all": {}}})
+            actions = []
             for analytic in analytics:
                 creation_dt = datetime.fromisoformat(analytic['_source']['result']['creationDt']) if analytic['_source'].get('result') is not None else datetime.now()
                 new_index = f"analytic-{creation_dt.strftime('%Y-%m-%d')}"
-                es.index(index=new_index, doc_type=analytic['_type'], body=analytic['_source'])
+                if index != new_index:
+                    actions.append({**{
+                        '_op_type': 'index',
+                        '_index': new_index,
+                        '_type': analytic['_type']
+                    }, **analytic['_source']})
+            bulk(es, actions)
             es.indices.delete(index)
 
     @property
